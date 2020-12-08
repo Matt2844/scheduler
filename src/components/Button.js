ece656255e2f97a944b19e9a91898b9502404743
Matthew  Ford <matthewford@Matthews-MacBook-Pro.local>
2020-12-08 13:53:02 -0500

    built daylist and daylistitem function w storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
+import classNames from 'classnames';
 
-export default function Button(props) {
-   return <></>;
+
+
+
+export default function Button (props) {
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
+
+   return (
+      <button
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   );
 }
