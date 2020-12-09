b0a7a720bc5b0d88268dd7362ab9c464de3d8f17
Matthew  Ford <matthewford@Matthews-MacBook-Pro.local>
2020-12-09 17:45:05 -0500

    scheduler all rough copies of components done so far

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,78 @@
 import React, { useState } from "react";
 
+import "components/Application.js"
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "components/Appointment/index.js"
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Jon Example",
+      interviewer: {
+        id: 3,
+        name: "Tony Palmer",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "12pm",
+  },
+  {
+    id: 5,
+    time: "3pm",
+    interview: {
+      student: "Bob Example",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  },
+  {
+    id: 7,
+    time: "4pm",
+  },
+  {
+    id: 8,
+    time: "4pm",
+    interview: {
+      student: "Sam Example",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  },
+];
+
+
 
 
 const days = [
@@ -48,7 +119,12 @@ export default function Application (props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => (
+          <Appointment
+            key={appointment.id} {...appointment}
+          />
+        ))}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+
+
+export default function Form (props) {
+  const [name, setName] = useState(props.name || '');
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  function reset () {
+    setName('');
+    setInterviewer(null);
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            onSubmit={(event) => event.preventDefault()}
+            placeholder="Enter Student Name" />
+        </form>
+        <InterviewerList interviewers={props.interviewers} value={interviewer} interviewer={interviewer} onChange={setInterviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={reset} onCancel={props.onCancel} >Cancel</Button>
+          <Button confirm onClick={props.onSave, props.name} name={props.student} interviewer>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
